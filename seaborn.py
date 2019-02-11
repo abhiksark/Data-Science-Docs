754bf07a3d44d3a723ed9b80f8049c3ec19f595c
Abhik <abhiksark@gmail.com>
2019-02-10 18:00:08 -0900

    .

--- seaborn.py
+++ seaborn.py
@@ -6,4 +6,4 @@ sns.boxplot(data=df,x='overall_rating',y='name')
 sns.kdeplot(x, shade=True, color="r") #kernel Density plot 
 sns.distplot(x, shade=True, color="r") #Histogram and Density Plot
 sns.barplot(x, shade=True, color="r")
-sns.countplot(x="class", data=titanic)
+sns.countplot(x="class", data=titanic) #Count Plot
