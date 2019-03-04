508ff90134fdc2142370f2e847e1d87950456606
Abhik <abhiksark@gmail.com>
2019-03-04 10:13:54 +0530

    added plot_size

--- aid/preprocessing/check.py
+++ aid/preprocessing/check.py
@@ -132,4 +132,11 @@ def reduce_mem_usage(df):
                 else:
                     df[col] = df[col].astype(np.float64)
         return df
- 
+ def set_plot_sizes(sml, med, big):
+    plt.rc('font', size=sml)          # controls default text sizes
+    plt.rc('axes', titlesize=sml)     # fontsize of the axes title
+    plt.rc('axes', labelsize=med)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=sml)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=sml)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=sml)    # legend fontsize
+    plt.rc('figure', titlesize=big)  # fontsize of the figure title
